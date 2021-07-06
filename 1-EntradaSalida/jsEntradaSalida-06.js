5ad39df27fa136a56ebd6db70a859cf104e323c8
danilovukosich <vukosichdanilo@gmail.com>
2021-07-05 21:11:43 -0300

    quinto commit

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,23 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
-}
+	var num1;
+	var num2;
+	var resultado;
 
+	num1 = document.getElementById('txtIdNumeroUno').value;//parseInt(document.getElementById('txtIdNumeroUno'));
+	num1 = parseInt(num1);
+
+	num2 = document.getElementById('txtIdNumeroDos').value;//parseInt(document.getElementById('txtIdNumeroDos').value);
+	num2 = parseInt(num2);
+
+	resultado = num1 + num2;
+
+
+	alert("La suma es igual a: " + resultado);
+
+
+	}
+
+//txtIdNumeroUno
+//txtIdNumeroDos
--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -5,5 +5,18 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	var num1;
+	var num2;
+	var resulltado;
+	var resto;
+
+	num1 = parseInst(document.getElementById('').value);
+	num2 = parseInt(document.getElementById('').value);
+
+	resultado = num1 / num2;
+
+	resto =
+
+
+	alert("El resto es: " + resto);
 }
